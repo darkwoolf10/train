123a1d64f968187add413e963285584807586125
Pasha Fud'ko <woolftreuser@gmail.com>
2019-10-21 00:12:58 +0300

    Added button with ui material component

--- train/resources/js/components/Hello.js
+++ train/resources/js/components/Hello.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 
 export default class Hello extends Component {
     render() {
         return (
             <div className="container">
                 HELLO MY FRIEND!
+                <br/>
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
             </div>
         );
     }
