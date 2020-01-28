26801e0c038946f485c29a212f1d1b7ec0b944cf
woolf <woolf@debian.woolf>
2020-01-28 22:53:46 +0200

    added relationship for routes

--- train/app/Http/Controllers/Api/RouteController.php
+++ train/app/Http/Controllers/Api/RouteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Route;
 use App\Station;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -20,6 +21,10 @@ class RouteController extends Controller
     {
         $routes = Route::all();
 
+        foreach ($routes as $route) {
+            $route['from'] = $route->from;
+        }
+
         return response()->json([
             'response' => $routes
         ]);
@@ -33,17 +38,26 @@ class RouteController extends Controller
     {
         $from = Station::where('city', $request->get('from'));
         $to = Station::where('city', $request->get('to'));
+        $date = Carbon::parse($request->get('date'))->format('Y/m/d');
 
         $routes = Route::where('from_id', '=', $from->id)->andWhere('to_id', '=', $to->id)->get();
+        $findRoutes = [];
 
-        if (!$routes) {
+        foreach ($routes as $route) {
+            $departureDate = Carbon::parse($route['departure_time'])->format('Y/m/d');
+            if ($departureDate === $date) {
+                $findRoutes[] = $route;
+            }
+        }
+
+        if (!$findRoutes) {
             return response()->json([
                 'error' => 'Not found routes',
             ], 400);
         }
 
         return response()->json([
-            'response' => $routes,
+            'response' => $findRoutes,
         ]);
     }
 
